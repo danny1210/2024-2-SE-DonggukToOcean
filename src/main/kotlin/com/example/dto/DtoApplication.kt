700acddbc741b9835f234a3c41f517335c9da7c8
danny1210 <danny1210@dgu.ac.kr>
2024-09-26 20:05:45 +0900

    initialize

---
+++ src/main/kotlin/com/example/dto/DtoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DtoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DtoApplication>(*args)
+}
